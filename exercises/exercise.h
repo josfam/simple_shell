ed8e5e3c8a90594a8a306c17fd89c2d30d65dad0
josfam <josephamukun@tutanota.com>
2023-12-04 13:47:26 +0300

    Remove prototype of main function

--- exercises/exercise.h
+++ exercises/exercise.h
@@ -5,9 +5,6 @@
 /* Returns the PID of the parent process */
 pid_t _getpid(void);
 
-/* Prints command-line arguments */
-int main(int ac, char **av);
-
 /* Writes a character to standard out */
 void _putchar(char ch);
 
