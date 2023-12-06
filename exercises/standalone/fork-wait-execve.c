a1b8048496ce56c733ced6327bf33c8bd5384621
josfam <josephamukun@tutanota.com>
2023-12-06 22:46:01 +0300

    Add requirement-fulfilling implementation

--- exercises/standalone/fork-wait-execve.c
+++ exercises/standalone/fork-wait-execve.c
@@ -5,34 +5,42 @@
 
 /**
  * main - fork example
- * 
+ *
  * Return: Always 0.
 */
 int main(void)
 {
+	const int MAX_PROCESSES = 5;
 	pid_t fork_num;
 	int status;
-	
+	int i;
+
 	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
-	fork_num = fork();
 
-	if (fork_num == -1)
+	for (i = 0; i < MAX_PROCESSES; i++)
 	{
-		perror("Error:");
-		return (1);
-	}
+		fork_num = fork();
 
-	if (fork_num == 0) /* This is a child process */
-	{
-		if (execve(argv[0], argv, __environ) == -1)
+		if (fork_num == -1)
 		{
 			perror("Error:");
+			return (1);
 		}
-		_exit(0); /* For proper termination of child process after a fail */
-	}
-	else
-	{
-		wait(&status);
+
+		if (fork_num == 0) /* This is a child process */
+		{
+			sleep(2);
+			if (execve(argv[0], argv, __environ) == -1)
+			{
+				perror("Error:");
+			}
+			_exit(0); /* For proper termination of child process after a fail */
+		}
+		else
+		{
+			/* waiting for this process to finish */
+			wait(&status);
+		}
 	}
 
 	return (0);
