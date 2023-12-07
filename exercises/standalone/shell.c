e850d7c57f1928c1f352e21cb15e1605bc6bef91
josfam <josephamukun@tutanota.com>
2023-12-07 11:49:30 +0300

    Add naive implementation of simple shell.
    This implementation repeatedly shows the prompt until EOF (CTRL + D) is
    entered.

---
+++ exercises/standalone/shell.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point.
+ * Description: The main entry point of the program.
+ * Return: 0 if successful, Non-zero otherwise.
+ */
+int main(void)
+{
+	size_t chars_read;
+	char *input_buff;
+	size_t buff_size;
+
+	/* setup defaults for getline */
+	chars_read = 0;
+	input_buff = NULL;
+	buff_size = 0;
+
+	while (1)
+	{
+		/* show the prompt */
+		printf("#cisfun$ ");
+		chars_read = getline(&input_buff, &buff_size, stdin);
+
+		/* error and EOF handling */
+		if (chars_read == -1)
+		{
+			free(input_buff);
+			break;
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
