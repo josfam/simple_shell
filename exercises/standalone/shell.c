6a7e66d1a7a69c89d837775a633f8adf04fdd7e8
josfam <josephamukun@tutanota.com>
2023-12-07 13:36:09 +0300

    Add shell code to execute the command once.

--- exercises/standalone/shell.c
+++ exercises/standalone/shell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 /**
  * main - Entry point.
@@ -12,6 +13,8 @@ int main(void)
 	ssize_t chars_read;
 	char *input_buff;
 	size_t buff_size;
+	char *command;
+	char *argv[] = {NULL};
 
 	/* setup defaults for getline */
 	chars_read = 0;
@@ -30,7 +33,24 @@ int main(void)
 			free(input_buff);
 			break;
 		}
+
+		/* accept no input */
+		if (chars_read == 1)
+			continue;
+
+		/* replace the newline added by getline, for execve's sake */
+		if (input_buff[chars_read - 1] == '\n')
+			input_buff[chars_read - 1] = '\0';
+
+		command = input_buff;
+
+		/* execute the command */
+		if (execve(command, argv, __environ) == -1)
+		{
+			perror("Execution error");
+		}
 	}
+
 	printf("\n");
 
 	return (0);
