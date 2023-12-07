590f3d093ad27dcb27c7055badf25815ec63cf27
josfam <josephamukun@tutanota.com>
2023-12-07 14:47:52 +0300

    Move prompt getting to its own function.
    To fix Betty's line count in a function warning, the act of
    showing the prompt and reading user input has been moved
    to its own function.

--- exercises/standalone/shell.c
+++ exercises/standalone/shell.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+char *get_prompt_input(void);
+
 /**
  * main - Entry point.
  * Description: The main entry point of the program.
@@ -10,11 +12,39 @@
  */
 int main(void)
 {
+	char *argv[] = {NULL};
+	char *user_input;
+
+	user_input = get_prompt_input();
+
+	if (!user_input)
+		return (-1);
+
+	/* execute command entered by the user */
+	if (execve(user_input, argv, __environ) == -1)
+	{
+		perror("Execution error");
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
+/**
+ * get_prompt_input - Shows a terminal prompt, and gets terminal
+ *                    inputs provided at said prompt.
+ * Description: Shows a terminal prompt, and gets terminal
+ *              inputs provided at said prompt.
+ *              If no input is provided, the user is re-prompted.
+ *              If CTRL+D is pressed, the program terminates.
+ * Return: The input entered (if any), or NULL on error.
+*/
+char *get_prompt_input(void)
+{
 	ssize_t chars_read;
 	char *input_buff;
 	size_t buff_size;
-	char *command;
-	char *argv[] = {NULL};
 
 	/* setup defaults for getline */
 	chars_read = 0;
@@ -31,27 +61,19 @@ int main(void)
 		if (chars_read == -1)
 		{
 			free(input_buff);
-			break;
+			return (NULL);
 		}
 
-		/* accept no input */
+		/* re-prompt if no input was provided */
 		if (chars_read == 1)
 			continue;
-
-		/* replace the newline added by getline, for execve's sake */
-		if (input_buff[chars_read - 1] == '\n')
-			input_buff[chars_read - 1] = '\0';
-
-		command = input_buff;
-
-		/* execute the command */
-		if (execve(command, argv, __environ) == -1)
-		{
-			perror("Execution error");
-		}
+		else
+			break;
 	}
 
-	printf("\n");
+	/* replace the newline added by getline, for execve's sake */
+	if (input_buff[chars_read - 1] == '\n')
+		input_buff[chars_read - 1] = '\0';
 
-	return (0);
+	return (input_buff);
 }
