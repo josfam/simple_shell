b3648a860acb5305f70bd4a4c50d314ce84ff100
josfam <josephamukun@tutanota.com>
2023-12-08 21:20:42 +0300

    Add version that only prints the PATH variable
    Instead of printing all the environment variables, this version
    of the program finds and prints just the PATH variable.

--- exercises/standalone/file-in-path.c
+++ exercises/standalone/file-in-path.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+int has_key(char *environ_var, char *key);
 
 /**
  * main - Entry point.
@@ -21,13 +24,49 @@ int main(int argc, char *argv[])
 		return (-1);
 	}
 
-	/* print the elements in the environment */
+	/* Extract and print the PATH variable from the environment */
 	while (*env != NULL)
 	{
 		env_item = *env;
-		printf("%s\n", env_item);
+		if (has_key(env_item, "PATH"))
+			printf("%s\n", env_item);
 		env++;
 	}
-	
+
 	return (0);
+}
+
+/**
+ * has_key - Returns whether or not the provided environment variable has the
+ *           provided key.
+ * @environ_var: The environment variable to check.
+ * @key: The key to check for.
+ * Description: Returns whether or not the provided environment variable has
+ *              the provided key.
+ * Return: 1 if the environment variable has the provided key, 0 if it does
+ *         not.
+ */
+int has_key(char *environ_var, char *key)
+{
+	int key_len, env_var_len, i;
+
+	key_len = strlen(key);
+	env_var_len = strlen(environ_var);
+
+	/* Eliminate non-matching environment variables */
+	if (key_len >= env_var_len)
+		return (0);
+
+	for (i = 0; i < key_len; i++)
+	{
+		if (environ_var[i] != key[i])
+			return (0);
+	}
+
+	/*eliminate environment variables that just happen to begin with the */
+	/* characters in the key */
+	if (environ_var[i] != '=')
+		return (0);
+
+	return (1);
 }
