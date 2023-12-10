ca396c1a946b1a8166fc10cf679cdf46ad11b0af
josfam <josephamukun@tutanota.com>
2023-12-10 20:51:44 +0300

    Move forking & execution logic to its own function

--- shell.c
+++ shell.c
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+int _execute(char *command, char **argv, char **env);
+
 /**
  * main - Entry point.
  * Description: The main entry point of the program.
@@ -13,34 +15,15 @@
 int main(void)
 {
 	char *input;
-	pid_t fork_num;
-	int wait_status;
-	char *argv[] = {NULL};
+	char *args[] = {NULL};
 
 	while (1)
 	{
 		input = _get_prompt_input();
 		if (!input) /* on error, or CTRL + D*/
 			break;
-
-		fork_num = fork();
-		if (fork_num == -1)
-		{
-			perror("Error");
+		if (_execute(input, args, __environ) == -1)
 			break;
-		}
-		if (fork_num == 0) /* Child process */
-		{
-			if (execve(input, argv, __environ) == -1)
-			{
-				perror("Error");
-			}
-			_exit(0);
-		}
-		else
-		{
-			wait(&wait_status);
-		}
 	}
 
 	_puts("\n");
@@ -51,3 +34,37 @@ int main(void)
 	return (0);
 }
 
+/**
+ * _execute - Executes a program/command, inside of a child process.
+ * @command: The program/command to execute.
+ * @args: Command line arguments for the program being executed.
+ * @env: Environment variables to be passed to the program.
+ * Description: Executes a program/command, inside of a child process.
+ * Return: 0 on success, -1 on error.
+*/
+int _execute(char *command, char **args, char **env)
+{
+	pid_t fork_num;
+	int wait_status;
+
+	fork_num = fork();
+	if (fork_num == -1)
+	{
+		perror("Error");
+		return (-1);
+	}
+	if (fork_num == 0) /* Child process */
+	{
+		if (execve(command, args, env) == -1)
+		{
+			perror("Error");
+		}
+		_exit(0);
+	}
+	else
+	{
+		wait(&wait_status);
+	}
+
+	return (0);
+}
