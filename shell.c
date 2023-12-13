a038b1fd3db8003a14024ace1522f7e17828773d
josfam <josephamukun@tutanota.com>
2023-12-13 16:51:56 +0300

    Formatting: Add white space for readability

--- shell.c
+++ shell.c
@@ -28,13 +28,16 @@ int main(__attribute__((unused)) int argc, char **argv, char **env)
 		/* on input error, CTRL + D*/
 		if (!input)
 			break;
+
 		args = _get_tokens(input, " ");
 		directories = malloc(MAX_DIRECTORIES_LEN);
+
 		if (directories == NULL)
 		{
 			perror("Error allocating memory");
 			return (EXIT_FAILURE);
 		}
+
 		env_path = get_path();
 		if (env_path == NULL)
 		{
@@ -43,25 +46,33 @@ int main(__attribute__((unused)) int argc, char **argv, char **env)
 			free(directories);
 			return (EXIT_FAILURE);
 		}
+
 		_strncpy(directories, env_path, MAX_DIRECTORIES_LEN - 1);
 		directories[MAX_DIRECTORIES_LEN - 1] = '\0';
 		free(env_path);
 		command = args[0];
 		executable_path = find_executable(command, directories);
+
 		if (executable_path == NULL)
 		{
 			perror("Error finding executable");
 			return (EXIT_FAILURE);
 		}
+
 		if (_execvp(executable_path, args) == -1)
+
 		if (_is_exit_call(args))
 			__exit();
+
 		if (_exec_in_child(args, argv, env) == -1)
 			break;
 	}
+
 	_puts("\n");
+
 	/* handle memory leaks */
 	free(input);
 	free(directories);
+
 	return (0);
 }
