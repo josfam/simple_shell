578f4ebd9ef748eb63b3b1b3600eff49a3ecea76
josfam <josephamukun@tutanota.com>
2023-12-13 17:20:18 +0300

    Docs & formatting: Add comments and whitespace

--- find_executable.c
+++ find_executable.c
@@ -13,6 +13,7 @@ int is_executable(char *path)
 {
 	return (access(path, X_OK) == 0);
 }
+
 /**
  * get_path - gets the environment path
  * Return: pointed to the string containing the path
@@ -23,11 +24,14 @@ char *get_path()
 
 	for (env = __environ; *env; ++env)
 	{
+		/* Find the pair PATH=..., and return a pointer to the start of */
+		/* the value after the = sign */
 		if (_strcmpr(*env, "PATH=", 5) == 0)
 			return (_strdup(*env + 5));
 	}
 	return (NULL);
 }
+
 /**
  * find_executable - finds the executable in path
  * @command: the command to be executed
@@ -41,17 +45,22 @@ char *find_executable(char *command, char *directories)
 	char *tmp_path;
 	char *tmp;
 
+	/* Return the original command, if it is a path to some folder */
 	if (_strchr(command, '/') != NULL)
 		return (_strdup(command));
+
 	while (token != NULL)
 	{
 		tmp_path = concatenate_strings(token, "/");
+
 		if (tmp_path == NULL)
 		{
 			return (NULL);
 		}
+
 		if (path == NULL)
 		{
+			/* Form a string of the pattern <directory-path>/command */
 			path = concatenate_strings(tmp_path, command);
 			if (path == NULL)
 			{
@@ -59,8 +68,10 @@ char *find_executable(char *command, char *directories)
 				return (NULL);
 			}
 		}
+
 		else
 		{
+			/* Form a string of the pattern <directory-path>/command */
 			tmp = concatenate_strings(path, command);
 			if (tmp == NULL)
 			{
@@ -71,16 +82,23 @@ char *find_executable(char *command, char *directories)
 			free(path);
 			path = tmp;
 		}
+
+		/* return the full path to the executable if the path indeed points */
+		/* to a an executable */
 		if (is_executable(path))
 		{
 			free(tmp_path);
 			return (path);
 		}
+
+		/* prepare to search the next directory for the executable */
 		free(tmp_path);
 		free(path);
 		path = NULL;
-		token = strtok(NULL, ":");
+		token = strtok(NULL, ":"); /* get the next directory in directories */
 	}
+
+	/* the executable was not found in the PATH */
 	free(path);
 	return (NULL);
 }
