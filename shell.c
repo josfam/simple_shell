c789a066290789363eae589fcd635e3be2f641dc
josfam <josephamukun@tutanota.com>
2023-12-15 15:14:31 +0300

    Free memory as soon as it is not required
    Instead of relying on a "catchall" free at the end of the while loop,
    memory is now being explicitly freed as soon as it is not required
    and on errors, of course.

--- shell.c
+++ shell.c
@@ -32,17 +32,16 @@ int main(__attribute__((unused)) int argc, char **argv, char **env)
 			break;
 
 		args = _get_tokens(input, " ");
-		free(input);
 
-		if (_is_exit_call(args))
+		if (_is_exit_call(args)) /* exit immediately */
 		{
-			/* exit immediately */
 			free(args);
 			__exit();
 		}
 
 		dir_mem = get_dir_mem(MAX_DIR_LEN, argv[0], input, args);
 		env_path = get_path_dirs(argv[0], input, dir_mem, args);
+		free(input);
 
 		_strncpy(dir_mem, env_path, MAX_DIR_LEN - 1);
 		dir_mem[MAX_DIR_LEN - 1] = '\0';
@@ -52,24 +51,22 @@ int main(__attribute__((unused)) int argc, char **argv, char **env)
 
 		if (executable_path == NULL)
 		{
-			free(executable_path);
+			_free_all(3, executable_path, dir_mem, args);
 			perror("Error finding executable");
 			return (EXIT_FAILURE);
 		}
 
 		if (_execvp(executable_path, args, argv, env) == -1)
 		{
-			free(executable_path);
+			_free_all(3, executable_path, dir_mem, args);
 			break;
 		}
-		free(executable_path);
+		_free_all(3, executable_path, dir_mem, args);
 	}
 
 	if (is_interactive)
 		_puts("\n");
 
-	/* handle memory leaks */
-	_free_all(3, args, input, dir_mem);
 
 	return (0);
 }
