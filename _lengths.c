128fc3fcd6573901f8a415b44d1c668e3f2dbe83
josfam <josephamukun@tutanota.com>
2023-12-31 18:13:20 +0300

    Move `_strlen` into `_lengths.c`

--- _lengths.c
+++ _lengths.c
@@ -35,3 +35,26 @@ int len_arr(char **arr)
 
 	return (len);
 }
+
+/**
+ * _strlen - Returns the length of a null-terminated string.
+ * @str: The string whose length to return.
+ * Description: Returns the length of a null-terminated string.
+ * Return: The length of the string, s.
+ */
+int _strlen(char *str)
+{
+	int len;
+	int counter;
+
+	counter = 0;
+	len = 0;
+
+	while (*(str + counter) != '\0')
+	{
+		len++;
+		counter++;
+	}
+
+	return (len);
+}
