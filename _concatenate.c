76b3520b576ad0db501095c88e580ae313724aaa
josfam <josephamukun@tutanota.com>
2023-12-31 17:48:21 +0300

    Move `_strcat` into `_concatenate.c`

--- _concatenate.c
+++ _concatenate.c
@@ -62,3 +62,24 @@ char *concat_three(char *s1, char *s2, char *s3)
 
 	return (fused);
 }
+
+/**
+ * _strcat - concatenates the string pointed to by src to the end
+ * of the string
+ *
+ * @dest: pointer to the string dest
+ * @src: pointer to the string src
+ * Return: pointer to the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	if (dest == NULL || src == NULL)
+		return (NULL);
+	while (*dest)
+		dest++;
+	while ((*dest++ = *src++))
+		;
+	return (ptr);
+}
